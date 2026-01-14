edb9b4e1efcf883aa9aa162896d8034899025799
Damien Levrault <damien.levrault@etu.u-bordeaux.fr>
2026-01-14 10:10:47 +0100

    enum

--- src/main/java/com/jad/EBaz.java
+++ src/main/java/com/jad/EBaz.java
@@ -1,4 +1,18 @@
 package com.jad;
 
-public enum EBaz {
+public enum EBaz implements IBaz {
+    BAZ1("Baz1"),
+    BAZ2("Baz2"),
+    BAZ3("Baz3");
+
+    private String name;
+
+    EBaz(String baz) {
+        this.name = baz;
+    }
+
+    @Override
+    public void doSomethingLikeABaz() {
+
+    }
 }
